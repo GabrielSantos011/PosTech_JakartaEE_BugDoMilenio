63cea9de17b7a42e9f81938fbdd081c72fb7ffa9
Gabriel <gabrielrafael.grs@gmail.com>
2023-10-10 18:07:47 -0300

    first commit

---
+++ src/main/java/br/com/fiap/bugdomilenio/ControleDataHora.java
@@ -0,0 +1,25 @@
+package br.com.fiap.bugdomilenio;
+
+import jakarta.ejb.EJB;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Named;
+
+
+@Named(value = "controleDataHora")
+@RequestScoped
+public class ControleDataHora {
+
+    @EJB
+    private BeanDataHora beanDataHora;
+
+    public ControleDataHora() {}
+
+
+    public BeanDataHora getBeanDataHora() {
+        return beanDataHora;
+    }
+
+    public void setBeanDataHora(BeanDataHora beanDataHora) {
+        this.beanDataHora = beanDataHora;
+    }
+}
